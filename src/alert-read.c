5af4d7a7999e6c9265d76b6297768f8d96a206eb
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2001-09-02 11:23:10 +0000

    * src/alert-read.c (prelude_alert_read_msg): 	* src/include/alert-read.h (prelude_alert_read_msg): 	buf is void.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@267 09c5ec92-17d4-0310-903a-819935f44dba

--- src/alert-read.c
+++ src/alert-read.c
@@ -95,7 +95,7 @@ alert_container_t *prelude_alert_read(int fd, uint8_t *tag)
 
 
 
-int prelude_alert_read_msg(alert_container_t *ac, uint8_t *tag, uint32_t *len, unsigned char **buf) 
+int prelude_alert_read_msg(alert_container_t *ac, uint8_t *tag, uint32_t *len, void **buf) 
 {
         if ( ac->msg_index == ac->max_msgcount ) 
                 return 0;
--- src/include/alert-read.h
+++ src/include/alert-read.h
@@ -1,6 +1,6 @@
 typedef struct alert_container alert_container_t;
 
-int prelude_alert_read_msg(alert_container_t *ac, uint8_t *tag, uint32_t *len, unsigned char **buf);
+int prelude_alert_read_msg(alert_container_t *ac, uint8_t *tag, uint32_t *len, void **buf);
 
 alert_container_t *prelude_alert_read(int fd, uint8_t *tag);
 
