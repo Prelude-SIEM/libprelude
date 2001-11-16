080f90096bf7eb834199e9f6859e0be4064c3b58
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2001-11-16 13:28:17 +0000

    * src/include/idmef-tree.h: IDMEF structure.         * src/include/idmef-message-id.h: ID for IDMEF based message.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@358 09c5ec92-17d4-0310-903a-819935f44dba

---
+++ src/include/idmef-message-id.h
@@ -0,0 +1,175 @@
+/*****
+*
+* Copyright (C) 2001 Yoann Vandoorselaere <yoann@mandrakesoft.com>
+* All Rights Reserved
+*
+* This file is part of the Prelude program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
+
+
+/*
+ * IDMEF message work...
+ */
+#define MSG_END_OF_TAG 254
+
+
+/*
+ * IDMEF alert
+ */
+#define MSG_ALERT_TAG 0
+#define MSG_ALERT_IDENT 1
+#define MSG_ALERT_ACTION 2
+#define MSG_ALERT_IMPACT 3
+#define MSG_ALERT_CREATE_TIME 4
+#define MSG_ALERT_DETECT_TIME 5
+#define MSG_ALERT_ANALYZER_TIME 6
+
+
+
+/*
+ * IDMEF source / target
+ */
+#define MSG_SOURCE_TAG 7
+#define MSG_SOURCE_IDENT 8
+#define MSG_SOURCE_SPOOFED 9
+#define MSG_SOURCE_INTERFACE 10
+
+
+/*
+ * IDMEF node
+ */
+#define MSG_NODE_TAG 11
+#define MSG_NODE_IDENT 12
+#define MSG_NODE_CATEGORY 13
+#define MSG_NODE_LOCATION 14
+#define MSG_NODE_NAME 15
+
+
+/*
+ * IDMEF address
+ */
+#define MSG_ADDRESS_TAG 16
+#define MSG_ADDRESS_IDENT 17
+#define MSG_ADDRESS_CATEGORY 18
+#define MSG_ADDRESS_VLAN_NAME 19
+#define MSG_ADDRESS_VLAN_NUM 20
+#define MSG_ADDRESS_ADDRESS 21
+#define MSG_ADDRESS_NETMASK 22
+
+
+
+/*
+ * IDMEF user
+ */
+#define MSG_USER_TAG 23
+#define MSG_USER_IDENT 24
+#define MSG_USER_CATEGORY 25
+
+
+/*
+ * IDMEF user Id
+ */
+#define MSG_USERID_TAG 26
+#define MSG_USERID_IDENT 27
+#define MSG_USERID_TYPE 28
+#define MSG_USERID_NAME 29
+#define MSG_USERID_NUMBER 30
+
+/*
+ * IDMEF process
+ */
+#define MSG_PROCESS_TAG 31
+#define MSG_PROCESS_IDENT 32
+#define MSG_PROCESS_NAME 33
+#define MSG_PROCESS_PID 34
+#define MSG_PROCESS_PATH 35
+#define MSG_PROCESS_ARG 36
+#define MSG_PROCESS_ENV 37
+
+
+/*
+ * IDMEF service
+ */
+#define MSG_SERVICE_TAG 38
+#define MSG_SERVICE_IDENT 39
+#define MSG_SERVICE_NAME 40
+#define MSG_SERVICE_PORT 41
+#define MSG_SERVICE_PORTLIST 42
+#define MSG_SERVICE_PROTOCOL 43
+
+
+/*
+ * IDMEF web service
+ */
+#define MSG_WEBSERVICE_TAG 44
+#define MSG_WEBSERVICE_URL 45
+#define MSG_WEBSERVICE_CGI 46
+#define MSG_WEBSERVICE_METHOD 47
+#define MSG_WEBSERVICE_ARG 48
+
+/*
+ * IDMEF snmp service
+ */
+#define MSG_SNMPSERVICE_TAG 49
+#define MSG_SNMPSERVICE_OID 50
+#define MSG_SNMPSERVICE_COMMUNITY 51
+#define MSG_SNMPSERVICE_COMMAND 52
+
+
+/*
+ * IDMEF classification
+ */
+#define MSG_CLASSIFICATION_TAG 53
+#define MSG_CLASSIFICATION_ORIGIN 54
+#define MSG_CLASSIFICATION_NAME 55
+#define MSG_CLASSIFICATION_URL 56
+
+/*
+ * IDMEF additional data
+ */
+#define MSG_ADDITIONALDATA_TAG 57
+#define MSG_ADDITIONALDATA_TYPE 58
+#define MSG_ADDITIONALDATA_MEANING 59
+#define MSG_ADDITIONALDATA_DATA 60
+
+
+/*
+ * IDMEF analyzer
+ */
+#define MSG_ANALYZER_TAG 61
+#define MSG_ANALYZER_ID  62
+#define MSG_ANALYZER_MANUFACTURER 63
+#define MSG_ANALYZER_MODEL 64
+#define MSG_ANALYZER_VERSION 65
+#define MSG_ANALYZER_CLASS 66
+
+
+/*
+ * IDMEF target
+ */
+#define MSG_TARGET_TAG 67
+#define MSG_TARGET_IDENT 68
+#define MSG_TARGET_DECOY 69
+#define MSG_TARGET_INTERFACE 70
+
+
+/*
+ * Other
+ */
+#define MSG_OWN_FORMAT 71
---
+++ src/include/idmef-tree.h
@@ -0,0 +1,455 @@
+/*****
+*
+* Copyright (C) 2001 Yoann Vandoorselaere <yoann@mandrakesoft.com>
+* All Rights Reserved
+*
+* This file is part of the Prelude program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
+
+#ifndef IDMEF_TREE_H
+#define IDMEF_TREE_H
+
+#include <inttypes.h>
+#include <libprelude/list.h>
+
+#define IDMEF_VERSION "0.5"
+
+
+/*
+ * Additional Data class
+ */
+typedef enum {
+        boolean   = 0,
+        byte      = 1,
+        character = 2,
+        date_time = 3,
+        integer   = 4,
+        ntpstamps = 5,
+        portlist  = 6,
+        real      = 7,
+        string    = 8,
+        xml       = 9,
+} idmef_additional_data_type_t;
+
+
+
+typedef struct {
+        struct list_head list;
+        idmef_additional_data_type_t type;
+        const char *meaning;
+        const char *data;
+} idmef_additional_data_t;
+
+
+
+
+
+/*
+ * Classification class
+ */
+typedef enum {
+        unknown         = 0,
+        bugtraqid       = 1,
+        cve             = 2,
+        vendor_specific = 3,
+} idmef_classification_origin_t;
+
+
+
+typedef struct {
+        struct list_head list;
+        idmef_classification_origin_t origin;
+        const char *name;
+        const char *url;
+} idmef_classification_t;
+
+
+
+
+
+/*
+ * UserId class
+ */
+typedef enum {
+        current_user  = 0,
+        original_user = 1,
+        target_user   = 2,
+        user_privs    = 3,
+        current_group = 4,
+        group_privs   = 5,
+} idmef_userid_type_t;
+
+
+typedef struct {
+        struct list_head list;
+        
+        const char *ident;
+        idmef_userid_type_t type;
+        const char *name;
+        const char *number;
+} idmef_userid_t;
+
+
+
+
+
+
+/*
+ * User class
+ */
+typedef enum {
+        cat_unknow  = 0,
+        application = 1,
+        os_device   = 2,
+} idmef_user_category_t;
+
+
+
+typedef struct {
+        const char *ident;
+        idmef_user_category_t category;
+        struct list_head userid_list;
+} idmef_user_t;
+
+
+
+
+/*
+ * Address class
+ */
+typedef enum {
+        addr_unknow   = 0,
+        atm           = 1,
+        e_mail        = 2,
+        lotus_notes   = 3,
+        mac           = 4,
+        sna           = 5,
+        vm            = 6,
+        ipv4_addr     = 7,
+        ipv4_addr_hex = 8,
+        ipv4_net      = 9,
+        ipv4_net_mask = 10,
+        ipv6_addr     = 11,
+        ipv6_addr_hex = 12,
+        ipv6_net      = 13,
+        ipv6_net_mask = 14,
+} idmef_address_category_t;
+
+
+
+
+typedef struct {
+        struct list_head list;
+        
+        const char *ident;
+        idmef_address_category_t category;
+        const char *vlan_name;
+        int vlan_num;
+        const char *address;
+        const char *netmask;
+} idmef_address_t;
+
+
+
+/*
+ * Process class
+ */
+typedef struct {
+        const char *ident;
+        const char *name;
+        const char *pid;
+        const char *path;
+        const char **arg;
+        const char **env;
+} idmef_process_t;
+
+
+
+/*
+ * WebService class
+ */
+typedef struct {
+        const char *url;
+        const char *cgi;
+        const char *method;
+        const char *arg;
+} idmef_webservice_t;
+
+
+
+
+/*
+ * SNMPService class
+ */
+typedef struct {
+        const char *oid;
+        const char *community;
+        const char *command;
+} idmef_snmpservice_t;
+
+        
+
+
+/*
+ * Service class
+ */
+typedef struct {
+        const char *ident;
+        const char *name;
+        uint16_t port;
+        const char *portlist;
+        const char *protocol;
+
+        union {
+                idmef_webservice_t *web;
+                idmef_snmpservice_t *snmp;
+        } specific;
+        
+} idmef_service_t;
+
+
+
+
+/*
+ * Node class
+ */
+typedef enum {
+        node_unknow = 0,
+        ads         = 1,
+        afs         = 2,
+        coda        = 3,
+        dfs         = 4,
+        dns         = 5,
+        kerberos    = 6,
+        nds         = 7,
+        nis         = 8,
+        nisplus     = 9,
+        nt          = 10,
+        wfw         = 11,
+} idmef_node_category_t;
+
+
+typedef struct {
+        const char *ident;
+        idmef_node_category_t category;
+        const char *location;
+        const char *name;
+        struct list_head address_list;
+} idmef_node_t;
+
+
+
+
+
+/*
+ * Source class
+ */
+typedef enum {
+        unknow = 0,
+        yes    = 1,
+        no     = 2,
+} idmef_spoofed_t;
+
+
+typedef struct {
+        struct list_head list;
+        
+        const char *ident;
+        idmef_spoofed_t spoofed;
+        const char *interface;
+
+        idmef_node_t node;
+        idmef_user_t user;
+        idmef_process_t process;
+        idmef_service_t service;
+        
+} idmef_source_t;
+
+
+
+
+/*
+ * Target class
+ */
+typedef struct {
+        struct list_head list;
+        
+        const char *ident;
+        idmef_spoofed_t decoy;
+        const char *interface;
+
+        idmef_node_t node;
+        idmef_user_t user;
+        idmef_process_t process;
+        idmef_service_t service;
+        
+} idmef_target_t;
+
+
+
+
+
+/*
+ * Analyzer class
+ */
+typedef struct {
+        const char *analyzerid;
+        const char *manufacturer;
+        const char *model;
+        const char *version;
+        const char *class;
+
+        idmef_node_t node;
+        idmef_process_t process;
+} idmef_analyzer_t;
+
+
+
+
+
+/*
+ * Time class
+ */
+typedef struct {
+        const char *ntpstamp;
+        const char *time;
+} idmef_time_t;
+
+
+
+
+/*
+ * Toolalert class
+ */
+typedef struct {
+        const char *name;
+        const char *command;
+        const char **analyzerid;
+} idmef_tool_alert_t;
+
+
+
+
+
+/*
+ * CorrelationAlert class
+ */
+typedef struct {
+        const char *name;
+        const char **alertident;
+} idmef_correlation_alert_t;
+
+
+
+
+/*
+ * OverflowAlert class
+ */
+typedef struct {
+        const char *program;
+        uint32_t size;
+        const unsigned char *buffer;
+} idmef_overflow_alert_t;
+
+
+
+
+/*
+ * Alert class
+ */
+typedef enum {
+        idmef_tool_alert        = 0,
+        idmef_correlation_alert = 1,
+        idmef_overflow_alert    = 2,
+} idmef_alert_type_t;
+
+
+
+typedef struct {
+        const char *ident;
+        const char *impact;
+        const char *action;
+    
+        idmef_analyzer_t analyzer;
+    
+        idmef_time_t create_time;
+        idmef_time_t detect_time;
+        idmef_time_t analyzer_time;
+
+        struct list_head source_list;
+        struct list_head target_list;
+        struct list_head classification_list;
+        struct list_head additional_data_list;
+
+        idmef_alert_type_t type;
+        union {
+                idmef_tool_alert_t *tool_alert;
+                idmef_correlation_alert_t *correlation_alert;
+                idmef_overflow_alert_t *overflow_alert;
+        } detail;
+        
+} idmef_alert_t;
+
+
+
+
+
+/*
+ * Heartbeat class
+ */
+typedef struct {
+        const char *ident;
+
+        idmef_analyzer_t analyzer;
+        idmef_time_t analyzer_time;
+
+        struct list_head additional_data_list;
+} idmef_heartbeat_t;
+
+
+
+
+/*
+ * IDMEF Message class
+ */
+typedef enum {
+        idmef_alert_message     = 0,
+        idmef_heartbeat_message = 1,
+} idmef_message_type_t;
+
+
+typedef struct {
+        const char *version;
+
+        idmef_message_type_t type;
+        union {
+                idmef_alert_t *alert;
+                idmef_heartbeat_t *heartbeat;
+        } message;
+        
+} idmef_message_t;
+
+#endif
+
+
+
+
+
+
+
