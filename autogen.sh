f64f3cc3da94a7bdc9cd8f1eb2d902ad388c19fc
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-10-01 01:48:57 +0000

    Use
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@4118 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,3 @@
-aclocal -I m4
-autoconf
-autoheader
-libtoolize -c --force --ltdl --automake
-automake --gnu -a -c
-#./configure
+autoreconf --install
+echo
+echo "You can now run \"./configure --enable-developer-mode\" and \"make\""
