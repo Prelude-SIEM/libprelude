0342c08a4fda51e04cbed87264ac33be71a5d93f
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-10-10 21:14:53 +0000

    New header file including most of the prelude header defining API and defining the library version.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@4165 09c5ec92-17d4-0310-903a-819935f44dba

---
+++ src/include/prelude.h
@@ -0,0 +1,46 @@
+/*****
+*
+* Copyright (C) 2004 Yoann Vandoorselaere <yoann@prelude-ids.org>
+* All Rights Reserved
+*
+* This file is part of the Prelude program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
+#ifndef _LIBPRELUDE_PRELUDE_H
+#define _LIBPRELUDE_PRELUDE_H
+
+#define LIBPRELUDE_VERSION "0.9.0-svn"
+
+#include "prelude-inttypes.h"
+#include "common.h"
+#include "prelude-client.h"
+#include "prelude-connection.h"
+#include "prelude-connection-mgr.h"
+#include "prelude-io.h"
+#include "prelude-getopt.h"
+#include "prelude-strbuf.h"
+#include "prelude-async.h"
+#include "prelude-plugin.h"
+#include "prelude-message.h"
+#include "prelude-message-buffered.h"
+#include "idmef.h"
+
+const char *prelude_check_version(const char *rq_version);
+
+
+#endif /* _LIBPRELUDE_PRELUDE_H */
