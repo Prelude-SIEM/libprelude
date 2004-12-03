8d68adcfc063444aaba98c3c72585589ade2a1be
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-12-03 18:04:27 +0000

    Commit implementation of libprelude-error, based on libgpg-error.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@4472 09c5ec92-17d4-0310-903a-819935f44dba

---
+++ src/libprelude-error/strerror.c
@@ -0,0 +1,50 @@
+/* strerror.c - Describing an error code.
+   Copyright (C) 2003 g10 Code GmbH
+
+   This file is part of libgpg-error.
+
+   libgpg-error is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+ 
+   libgpg-error is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+ 
+   You should have received a copy of the GNU Lesser General Public
+   License along with libgpg-error; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "prelude-error.h"
+#include "gettext.h"
+#include "err-codes.h"
+
+/* Return a pointer to a string containing a description of the error
+   code in the error value ERR.  This function is not thread-safe.  */
+const char *prelude_strerror(prelude_error_t err)
+{
+	int no;
+  	prelude_error_code_t code = prelude_error_get_code(err);
+        
+  	if ( code & PRELUDE_ERROR_SYSTEM_ERROR ) {
+      		no = prelude_error_code_to_errno(code);
+      		if ( no )
+			return strerror(no);
+      		else
+			code = PRELUDE_ERROR_UNKNOWN_ERRNO;
+    	}
+  
+  	return dgettext(PACKAGE, msgstr + msgidx[msgidxof(code)]);
+}
