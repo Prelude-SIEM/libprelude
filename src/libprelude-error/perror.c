f66d68653beead2d3b92a5e99afbac80babeb94b
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-12-17 01:54:53 +0000

    Define some more code. Implement prelude_perror().
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@4568 09c5ec92-17d4-0310-903a-819935f44dba

---
+++ src/libprelude-error/perror.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "prelude-log.h"
+#include "prelude-error.h"
+
+
+void prelude_perror(prelude_error_t error, const char *fmt, ...)
+{
+	va_list ap;
+	char buf[1024];
+
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	
+	log(LOG_INFO, "%s: %s: %s.\n", prelude_strsource(error), buf, prelude_strerror(error));
+}
