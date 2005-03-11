81a4c76ac03be27604484e3dfe44d3e88cac1546
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-03-11 15:02:42 +0000

    Warn and exit if gtkdocize is not available.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@5449 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 libtoolize -c --force --ltdl --automake
 autoreconf --install --force
-gtkdocize
+gtkdocize || echo "gtkdocize missing. Please install gtk-doc."; exit 1
 
 echo
 echo "You can now run \"./configure --enable-developer-mode\" and \"make\""
