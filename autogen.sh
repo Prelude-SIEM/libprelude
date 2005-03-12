4bf8c955a8a3e6cff9fa5ac561bd05a62e2d3262
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-03-12 03:06:44 +0000

    Move gtkdocize before anything else.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@5459 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
+gtkdocize --copy || echo "gtkdocize missing. Please install gtk-doc."; exit 1
 libtoolize -c --force --ltdl --automake
 autoreconf --install --force
-gtkdocize --copy || echo "gtkdocize missing. Please install gtk-doc."; exit 1
 
 echo
 echo "You can now run \"./configure --enable-developer-mode\" and \"make\""
