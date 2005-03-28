796417ba5ba74188129d45f02ed3b25908866639
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-03-28 01:30:53 +0000

    Always destroy the heartbeat timer, this fix a problem when using the FAILURE destroy status.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@5804 09c5ec92-17d4-0310-903a-819935f44dba

--- src/prelude-client.c
+++ src/prelude-client.c
@@ -253,7 +253,9 @@ static void heartbeat_expire_cb(void *data)
         
   out:
         idmef_message_destroy(message);
-        prelude_timer_reset(&client->heartbeat_timer);
+
+        if ( client->status != CLIENT_STATUS_EXITING )
+                prelude_timer_reset(&client->heartbeat_timer);
 }
 
 
@@ -1276,17 +1278,16 @@ void prelude_client_set_heartbeat_cb(prelude_client_t *client,
  * from a signal handler.
  */
 void prelude_client_destroy(prelude_client_t *client, prelude_client_exit_status_t status)
-{        
+{
         if ( status == PRELUDE_CLIENT_EXIT_STATUS_SUCCESS && client->flags & PRELUDE_CLIENT_FLAGS_HEARTBEAT ) {
-                prelude_timer_lock_critical_region();
-                
                 client->status = CLIENT_STATUS_EXITING;
                 heartbeat_expire_cb(client);
-                prelude_timer_destroy(&client->heartbeat_timer);
-
-                prelude_timer_unlock_critical_region();
         }
-
+        
+        prelude_timer_lock_critical_region();
+        prelude_timer_destroy(&client->heartbeat_timer);
+        prelude_timer_unlock_critical_region();
+        
         _prelude_client_destroy(client);
 }
 
