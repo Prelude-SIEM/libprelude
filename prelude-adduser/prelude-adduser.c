03e570c2e7832400b3b982d567c2e8006c97149e
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-05-26 13:00:21 +0000

    evert to using PF_UNSPEC for connection.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@6453 09c5ec92-17d4-0310-903a-819935f44dba

--- prelude-adduser/prelude-adduser.c
+++ prelude-adduser/prelude-adduser.c
@@ -400,10 +400,10 @@ static prelude_io_t *connect_manager(const char *addr, unsigned int port, char *
 #ifdef AI_ADDRCONFIG
         hints.ai_flags = AI_ADDRCONFIG;
 #endif
-
+        
+        hints.ai_family = PF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_protocol = IPPROTO_TCP;
-        hints.ai_family = find_default_family();
         
         ret = getaddrinfo(addr, buf, &hints, &ai);
         if ( ret != 0 ) {
