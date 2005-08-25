b134131c406635c728ff2be3eaf7aec45fc607e2
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-08-25 01:14:02 +0000

    Include config.h/libmissing.h
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@7082 09c5ec92-17d4-0310-903a-819935f44dba

--- src/idmef-class.c
+++ src/idmef-class.c
@@ -22,6 +22,7 @@
 *
 *****/
 
+#include "config.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,6 +30,7 @@
 #include <sys/types.h>
 #include <stdarg.h>
 
+#include "libmissing.h"
 #include "prelude-list.h"
 #include "prelude-log.h"
 #include "prelude-inttypes.h"
