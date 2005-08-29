e3ed9de1f2a6c223d7997adcccc96face91aa26d
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-08-29 09:10:24 +0000

    Use locale config.h.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@7106 09c5ec92-17d4-0310-903a-819935f44dba

--- src/libprelude-error/code-from-errno.c
+++ src/libprelude-error/code-from-errno.c
@@ -18,9 +18,7 @@
    Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA.  */
 
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
+#include "config.h"
 
 #include "prelude-error.h"
 #include "code-from-errno.h"
