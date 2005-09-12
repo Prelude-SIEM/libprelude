43dacaafbc660be915d03e151fe3f01d8c165ee8
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2005-09-12 14:41:26 +0000

    Slight improvement.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@7168 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,19 @@
-gtkdocize --copy 
+GNULIB_TOOL=`which gnulib-tool 2> /dev/null`
+if test -n "$GNULIB_TOOL"; then
+	echo "Running gnulib-tool..."
+        $GNULIB_TOOL --import > /dev/null
+fi
+
+GTKDOCIZE=`which gtkdocize 2> /dev/null`
+if test -n "$GTKDOCIZE"; then
+	echo "Running gtkdocize..."
+	$GTKDOCIZE --copy
+fi
+
+echo "Running libtoolize..."
 libtoolize -c --force --ltdl --automake
+
+echo "Running autoreconf..."
 autoreconf --install --force
 
 echo
