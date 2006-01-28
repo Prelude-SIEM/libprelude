cd67990b087c06604b7672438008ccd4f23a08f0
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2006-01-28 01:44:03 +0000

    Formatting trick. Only print the setup message in case we're not going to return the error to the upper layer.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@7640 09c5ec92-17d4-0310-903a-819935f44dba

--- src/prelude-connection-pool.c
+++ src/prelude-connection-pool.c
@@ -256,8 +256,9 @@ static void notify_dead(cnx_t *cnx, prelude_error_t error, prelude_bool_t init_t
         if ( cnx->is_dead )
                 return;
 
-        prelude_log(PRELUDE_LOG_WARN, "Failover enabled: connection error with %s: %s\n",
-                    prelude_connection_get_peer_addr(cnx->cnx), prelude_strerror(error));
+        if ( ! init_time || ! prelude_client_is_setup_needed(error) ) 
+                prelude_log(PRELUDE_LOG_WARN, "Failover enabled: connection error with %s: %s\n\n",
+                            prelude_connection_get_peer_addr(cnx->cnx), prelude_strerror(error));
         
         clist->dead++;
         cnx->is_dead = TRUE;
@@ -424,7 +425,7 @@ static void connection_timer_expire(void *data)
         
         ret = prelude_connection_connect(cnx->cnx, pool->client_profile, pool->permission);
         if ( ret < 0 ) {
-                prelude_log(PRELUDE_LOG_WARN, "Failover enabled: connection error with %s: %s\n",
+                prelude_log(PRELUDE_LOG_WARN, "Failover enabled: connection error with %s: %s\n\n",
                             prelude_connection_get_peer_addr(cnx->cnx), prelude_strerror(ret));
                                 
                 /*
