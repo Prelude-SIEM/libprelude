6426d866ddd9427811b0cbe2b88ab29c85952947
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2006-12-20 08:50:03 +0000

    Remove unnecessary sys/time.h inclusion. Use GnuLib poll module.
    git-svn-id: file:///home/yoann/SVN/trunk/libprelude@8678 09c5ec92-17d4-0310-903a-819935f44dba

--- prelude-adduser/server.c
+++ prelude-adduser/server.c
@@ -30,11 +30,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/poll.h>
 
 #include <gcrypt.h>
 #include <gnutls/gnutls.h>
