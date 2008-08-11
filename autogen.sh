d85a288a076ee5f1d91720133112fb8b83866386
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-08-11 12:39:30 +0000

    Exit in case of command failure.
    git-svn-id: file:///home/yoann/SVN/libprelude/trunk@10832 09c5ec92-17d4-0310-903a-819935f44dba

--- autogen.sh
+++ autogen.sh
@@ -3,20 +3,20 @@
 GNULIB_TOOL=`which gnulib-tool 2> /dev/null`
 if test -n "$GNULIB_TOOL"; then
 	echo "Running gnulib-tool..."
-        $GNULIB_TOOL --import > /dev/null
+        $GNULIB_TOOL --import > /dev/null || exit 1
 fi
 
 GTKDOCIZE=`which gtkdocize 2> /dev/null`
 if test -n "$GTKDOCIZE"; then
 	echo "Running gtkdocize..."
-	$GTKDOCIZE --copy
+	$GTKDOCIZE --copy || exit 1
 fi
 
 echo "Running libtoolize..."
-libtoolize -c --force --ltdl --automake
+libtoolize -c --force --ltdl --automake || exit 1
 
 echo "Running autoreconf..."
-autoreconf --install --force
+autoreconf --install --force || exit 1
 
 echo
 echo "You can now run \"./configure --enable-developer-mode\" and \"make\""
