f9fe9918ab705035d87fe58bb5422dd986e5e4ef
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-08-14 14:01:38 +0000

    New idmef-message-helper unit-test
    git-svn-id: file:///home/yoann/SVN/libprelude/trunk@10845 09c5ec92-17d4-0310-903a-819935f44dba

---
+++ tests/idmef-message-helper.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "prelude.h"
+
+#define FLOAT_TOLERANCE 0.0001
+
+#ifndef ABS
+# define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
+
+#ifndef MAX
+# define MAX(x, y) ((x) > (y) ? (x) : (y))
+#endif
+
+
+static double reldif(double a, double b)
+{
+        double c = ABS(a);
+        double d = ABS(b);
+
+        d = MAX(c, d);
+
+        return d == 0.0 ? 0.0 : ABS(a - b) / d;
+}
+
+
+int main(void)
+{
+        char *res;
+        double resd;
+        idmef_message_t *idmef;
+
+        assert(idmef_message_new(&idmef) == 0);
+
+        assert(idmef_message_set_string(idmef, "alert.classification.text", "Random value") == 0);
+        assert(idmef_message_get_string(idmef, "alert.classification.text", &res) == 0);
+        assert(strcmp("Random value", res) == 0);
+        free(res);
+
+        assert(idmef_message_set_string(idmef, "alert.assessment.impact.severity", "high") == 0);
+        assert(idmef_message_get_string(idmef, "alert.assessment.impact.severity", &res) == 0);
+        assert(strcmp("high", res) == 0);
+        free(res);
+
+        assert(idmef_message_set_number(idmef, "alert.assessment.confidence.confidence", 10.2) == 0);
+        assert(idmef_message_get_number(idmef, "alert.assessment.confidence.confidence", &resd) == 0);
+        assert(reldif(resd, 10.2) <= FLOAT_TOLERANCE);
+
+        assert(idmef_message_set_string(idmef, "alert.assessment.impact.severity", "Random value") < 0);
+
+        exit(0);
+}
