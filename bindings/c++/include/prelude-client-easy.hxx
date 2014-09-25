61b4f29b0b7a553479bb8846cc1fda4d9b65dcba
Yoann Vandoorselaere <yoannv@gmail.com>
2014-09-25 17:42:47 +0200

    Add missing copyright notice
    Change-Id: I1663d07008e7e36dcec8930d645e0017e0f701b3

--- bindings/c++/include/prelude-client-easy.hxx
+++ bindings/c++/include/prelude-client-easy.hxx
@@ -1,3 +1,26 @@
+/*****
+*
+* Copyright (C) 2009-2014 CS-SI. All Rights Reserved.
+* Author: Yoann Vandoorselaere <yoannv@gmail.com>
+*
+* This file is part of the Prelude library.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*****/
+
 #ifndef _LIBPRELUDE_PRELUDE_CLIENT_EASY_HXX
 #define _LIBPRELUDE_PRELUDE_CLIENT_EASY_HXX
 
@@ -18,10 +41,10 @@ namespace Prelude {
             public:
                 ClientEasy(const char *profile,
                            int permission = Client::IDMEF_WRITE,
-                           const char *_model = "Unknown model",
-                           const char *_class = "Unknown class",
-                           const char *_manufacturer = "Unknown manufacturer",
-                           const char *_version = "Unknown version");
+                           const char *model = "Unknown model",
+                           const char *classname = "Unknown class",
+                           const char *manufacturer = "Unknown manufacturer",
+                           const char *version = "Unknown version");
         };
 };
 
--- bindings/c++/include/prelude-client.hxx
+++ bindings/c++/include/prelude-client.hxx
@@ -1,3 +1,26 @@
+/*****
+*
+* Copyright (C) 2009-2014 CS-SI. All Rights Reserved.
+* Author: Yoann Vandoorselaere <yoannv@gmail.com>
+*
+* This file is part of the Prelude library.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*****/
+
 #ifndef _LIBPRELUDE_PRELUDE_CLIENT_HXX
 #define _LIBPRELUDE_PRELUDE_CLIENT_HXX
 
