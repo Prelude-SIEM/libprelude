6fd4107950b6df7289fae0378c3b09cdd7cc9099
Yoann Vandoorselaere <yoannv@gmail.com>
2015-05-05 16:37:38 +0700

    IDMEFCriteria broken down time matching fixes
    Lesser than and greater than operator were not working in combination
    with wday/yday relative matching as soon as other fields were involded.
    For example, this would always return false in case the current day was
    monday : alert.create_time < 'wday:monday hour:10'
    
    Additionally, there was plenty of matching problem with the lesser and
    greater operator.
    
    Include time/broken-down time unit test in tests/idmef-criteria.c.
    
    Change-Id: I5aefbb336a1ff6bf8d3d4eb185cc89a3dbd53d7a

--- tests/idmef-criteria.c
+++ tests/idmef-criteria.c
@@ -21,6 +21,7 @@ static void test_criteria(idmef_message_t *idmef, const char *criteria_str, int 
 
 int main(void)
 {
+        idmef_time_t *ctime;
         idmef_alert_t *alert;
         idmef_message_t *idmef;
         idmef_classification_t *classification;
@@ -54,6 +55,85 @@ int main(void)
         test_criteria(idmef, "alert.classification.text ~ 'My (String|Check)'", 0, 1);
         test_criteria(idmef, "alert.classification.text ~ 'my'", 0, 0);
         test_criteria(idmef, "alert.classification.text ~* 'my'", 0, 1);
+
+        idmef_alert_new_create_time(alert, &ctime);
+        assert(idmef_time_set_from_string(ctime, "2015-05-03 1:59:08") == 0);
+
+        /*
+         * Regular time operator check
+         */
+        test_criteria(idmef, "alert.create_time == '2015-05-03 1:59:08'", 0, 1);
+        test_criteria(idmef, "alert.create_time != '2015-05-03 1:59:08'", 0, 0);
+        test_criteria(idmef, "alert.create_time < '2015-05-03 1:59:08'", 0, 0);
+        test_criteria(idmef, "alert.create_time > '2015-05-03 1:59:08'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= '2015-05-03 1:59:08'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= '2015-05-03 1:59:08'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == '2015-05-03 1:59:07'", 0, 0);
+        test_criteria(idmef, "alert.create_time != '2015-05-03 1:59:07'", 0, 1);
+        test_criteria(idmef, "alert.create_time < '2015-05-03 1:59:07'", 0, 0);
+        test_criteria(idmef, "alert.create_time > '2015-05-03 1:59:07'", 0, 1);
+        test_criteria(idmef, "alert.create_time <= '2015-05-03 1:59:07'", 0, 0);
+        test_criteria(idmef, "alert.create_time >= '2015-05-03 1:59:07'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time < '2015-05-03 1:59:09'", 0, 1);
+        test_criteria(idmef, "alert.create_time > '2015-05-03 1:59:09'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= '2015-05-03 1:59:09'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= '2015-05-03 1:59:09'", 0, 0);
+
+        /*
+         * Broken down time check
+         */
+        assert(idmef_time_set_from_string(ctime, "2015-05-04 00:00:00+00:00") == 0);
+        test_criteria(idmef, "alert.create_time == 'month:may mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time != 'month:may mday:3'", 0, 1);
+        test_criteria(idmef, "alert.create_time < 'month:may mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time > 'month:may mday:3'", 0, 1);
+        test_criteria(idmef, "alert.create_time <= 'month:may mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time >= 'month:may mday:3'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == 'month:may mday:4'", 0, 1);
+        test_criteria(idmef, "alert.create_time != 'month:may mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time < 'month:may mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time > 'month:may mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= 'month:may mday:4'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= 'month:may mday:4'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == 'month:may mday:5'", 0, 0);
+        test_criteria(idmef, "alert.create_time != 'month:may mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time < 'month:may mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time > 'month:may mday:5'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= 'month:may mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= 'month:may mday:5'", 0, 0);
+
+        /*
+         * Broken down time special wday/yday fields
+         */
+        test_criteria(idmef, "alert.create_time == 'wday:monday'", 0, 1);
+        test_criteria(idmef, "alert.create_time != 'wday:monday'", 0, 0);
+        test_criteria(idmef, "alert.create_time == 'wday:tuesday'", 0, 0);
+        test_criteria(idmef, "alert.create_time != 'wday:tuesday'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == 'wday:monday mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time != 'wday:monday mday:3'", 0, 1);
+        test_criteria(idmef, "alert.create_time < 'wday:monday mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time > 'wday:monday mday:3'", 0, 1);
+        test_criteria(idmef, "alert.create_time <= 'wday:monday mday:3'", 0, 0);
+        test_criteria(idmef, "alert.create_time >= 'wday:monday mday:3'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == 'wday:monday mday:4'", 0, 1);
+        test_criteria(idmef, "alert.create_time != 'wday:monday mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time < 'wday:monday mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time > 'wday:monday mday:4'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= 'wday:monday mday:4'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= 'wday:monday mday:4'", 0, 1);
+
+        test_criteria(idmef, "alert.create_time == 'wday:monday mday:5'", 0, 0);
+        test_criteria(idmef, "alert.create_time != 'wday:monday mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time < 'wday:monday mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time > 'wday:monday mday:5'", 0, 0);
+        test_criteria(idmef, "alert.create_time <= 'wday:monday mday:5'", 0, 1);
+        test_criteria(idmef, "alert.create_time >= 'wday:monday mday:5'", 0, 0);
 
         exit(0);
 }
